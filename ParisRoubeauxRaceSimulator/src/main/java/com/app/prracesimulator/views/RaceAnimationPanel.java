f409be41e7767ba4445e358aad6fa91477a3b94e
yulianaboyaca04 <mary.boyaca@uptc.edu.co>
2020-02-09 18:55:09 -0500

    cambio draw

--- ParisRoubeauxRaceSimulator/src/main/java/com/app/prracesimulator/views/RaceAnimationPanel.java
+++ ParisRoubeauxRaceSimulator/src/main/java/com/app/prracesimulator/views/RaceAnimationPanel.java
@@ -24,91 +24,41 @@ import com.app.prracesimulator.models.entities.RaceConstants;
  */
 public class RaceAnimationPanel extends JPanel {
 
-	public ArrayList<Cyclist> racers;
-	public ArrayList<PaveSection> paveSegments;
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private RaceSketch raceSketch;
 
 	public RaceAnimationPanel(Controller controller) {
 		super();
 		setBackground(Color.red);		
-		this.racers = controller.getRace().getRacers();
-		this.paveSegments = controller.getRace().getPaveSegments();
-		this.initComponents();
-	}
-
-	private void initComponents() {
-		Toolkit t = Toolkit.getDefaultToolkit();
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		int sizeX = 1200 ;
-		int sizeY = 400 ;
-		System.out.println("ancho: " + sizeX + " aLTO: "+ sizeY);
-		RaceSketch raceSketch =  new RaceSketch();
-		this.setPreferredSize(new Dimension(sizeX, sizeY));
-		this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		raceSketch.setPreferredSize(new Dimension(sizeX,sizeY - 10));
-		this.add(raceSketch);
-		this.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-		this.setPreferredSize(new Dimension(sizeX,sizeY));
-	}
-
-//	@Override
-//	public void paint(Graphics g) {
-//		super.paint(g);
-//		Graphics2D g2D = (Graphics2D) g;
-//		//hay que pintar el terreno de la carrera
-//		this.paintGroundRace(g2D);
-//		//aca se pintan los ciclistas
-//		paintRacers(g2D);		
-//	}
-
-	/**
-	 * metodo en el que se pintan los racers deacuerdo a su ubicacion y su estado
-	 * @param g2D
-	 */
-	private void paintRacers(Graphics2D g2D) {
-		for (Cyclist cyclist : racers) {
-			// segun el estado del ciclista se debe pintar de un color u otro
-			switch (cyclist.getCyclistState()) {
-			case RACING:
-				g2D.setColor(Color.ORANGE);
-				break;
-			case FINISHER:
-				g2D.setColor(Color.GREEN);
-				break;
-			case DEQUALIFIED:
-				g2D.setColor(Color.RED);
-				break;
-
-			default:
-				break;
-			}
-			g2D.drawOval((int) cyclist.getLocation().getX(), // ubicacion en x del ovalo
-					(int) cyclist.getLocation().getY(),//ubicacion en y del ovalo
-						10,//ancho 
-					100);// alto
-
-		}
-	}
-	
-	/**
-	 * aca se debe pintar la carrera deacuerdo a los segmentos de pave
-	 * @param g2D
-	 */
-	private void paintGroundRace(Graphics2D g2D) {
 		
+		this.initComponents(controller);
 	}
+
+	private void initComponents(Controller controller) {
+		int sizeX = 1100 ;
+		int sizeY = 400 ;		
+		raceSketch =  new RaceSketch(controller, sizeX, sizeY);
+		//this.setPreferredSize(new Dimension(sizeX, sizeY));
+		this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		raceSketch.setPreferredSize(new Dimension(sizeX,sizeY));
+		this.add(raceSketch);
+		
+		//this.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		//this.setPreferredSize(new Dimension(sizeX,sizeY));
+	}
+
+
 
 	/**
 	 * metodoi quye se llama desde el controler y actualiza la posicion de los racers
 	 * @param racers
 	 */
 	public void setRacersPositions(ArrayList<Cyclist> racers) {
-		this.racers = racers;
-		this.repaint();//esto se tiene que dejar al final de todo el dibujo que se haga
-
+		this.raceSketch.setRacersPositions(racers);
 	}
 }
--- ParisRoubeauxRaceSimulator/src/main/java/com/app/prracesimulator/views/RaceSketch.java
+++ ParisRoubeauxRaceSimulator/src/main/java/com/app/prracesimulator/views/RaceSketch.java
@@ -3,8 +3,13 @@ package com.app.prracesimulator.views;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
+import com.app.prracesimulator.controllers.Controller;
+import com.app.prracesimulator.models.entities.Cyclist;
+import com.app.prracesimulator.models.entities.PaveSection;
 import com.app.prracesimulator.models.entities.RaceConstants;
 
 /**
@@ -19,15 +24,19 @@ public class RaceSketch extends Canvas {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
-	private int pixelesRuta = 2000;
+	public ArrayList<Cyclist> racers;
+	public ArrayList<PaveSection> paveSegments;
+	
+	private int pixelesRuta = 0;
 	
 	/**
 	 * Constructor de la clase, que inicializa todos los valores necesarios para el
 	 * pintado de la carrera.
 	 */
-	public RaceSketch() {
-		
+	public RaceSketch(Controller controller, int sizeX, int sizeY) {
+		this.pixelesRuta = sizeX;
+		this.racers = controller.getRace().getRacers();
+		this.paveSegments = controller.getRace().getPaveSegments();
 	}
 
 	/**
@@ -51,8 +60,48 @@ public class RaceSketch extends Canvas {
 
 		// Actualiza el buffer en el canvas
 		g.drawImage(buffer, 0, 0, this);
+		
+
+		drawCyclists(g);	
 	}
 
+	private void drawCyclists(Graphics g) {
+		Graphics2D g2D = (Graphics2D) g;
+		//hay que pintar el terreno de la carrera
+		this.paintGroundRace(g2D);
+		//aca se pintan los ciclistas
+		paintRacers(g2D);
+	}
+
+	/**
+	 * metodo en el que se pintan los racers deacuerdo a su ubicacion y su estado
+	 * @param g2D
+	 */
+	private void paintRacers(Graphics2D g2D) {
+		for (Cyclist cyclist : racers) {
+			// segun el estado del ciclista se debe pintar de un color u otro
+			switch (cyclist.getCyclistState()) {
+			case RACING:
+				g2D.setColor(Color.ORANGE);
+				break;
+			case FINISHER:
+				g2D.setColor(Color.GREEN);
+				break;
+			case DEQUALIFIED:
+				g2D.setColor(Color.RED);
+				break;
+
+			default:
+				break;
+			}
+			g2D.drawOval((int) cyclist.getLocation().getX(), // ubicacion en x del ovalo
+					(int) cyclist.getLocation().getY(),//ubicacion en y del ovalo
+						10,//ancho 
+					100);// alto
+
+		}
+	}
+	
 	/**
 	 * Dibuja el cielo sobre el buffer (Graphics) que sirve como imagen de fondo.
 	 * 
@@ -90,12 +139,31 @@ public class RaceSketch extends Canvas {
 		for (int i = 0; i < numerodelineas + 1; i++) {
 			g.drawLine((int) (i * distanciapixelsentrelineas), (int) (700), //X
 					(int) (i * distanciapixelsentrelineas), (int) (400 - 15));//Y
-			g.drawString(Integer.toString(i * distancia), (int) (i * distanciapixelsentrelineas) - 5,
+			g.drawString(Integer.toString(i * distancia), (int) (i * distanciapixelsentrelineas),
 					(int) (400 - 18)); //Y
 		}
 	}
 
 
 
+	
+	
+	/**
+	 * aca se debe pintar la carrera deacuerdo a los segmentos de pave
+	 * @param g2D
+	 */
+	private void paintGroundRace(Graphics2D g2D) {
+		
+	}
+
+	/**
+	 * metodoi quye se llama desde el controler y actualiza la posicion de los racers
+	 * @param racers
+	 */
+	public void setRacersPositions(ArrayList<Cyclist> racers) {
+		this.racers = racers;
+		this.repaint();//esto se tiene que dejar al final de todo el dibujo que se haga
+		
+	}
 
 }
