4df2678021b6d49f591d34ea05b124b92005ea71
yulianaboyaca04 <mary.boyaca@uptc.edu.co>
2020-02-11 23:39:38 -0500

    cambios y ajustes vista

--- ParisRoubeauxRaceSimulator/src/main/java/com/app/prracesimulator/views/RaceAnimationPanel.java
+++ ParisRoubeauxRaceSimulator/src/main/java/com/app/prracesimulator/views/RaceAnimationPanel.java
@@ -10,6 +10,7 @@ import javax.swing.JPanel;
 
 import com.app.prracesimulator.controllers.Controller;
 import com.app.prracesimulator.models.entities.Cyclist;
+import com.app.prracesimulator.models.entities.RaceConstants;
 
 /**
  * Panel que contiene el canvas de la carrera
@@ -25,12 +26,11 @@ public class RaceAnimationPanel extends JPanel {
 	
 	private RaceSketch raceSketch;
 	
-	private static final int DistanciaCarreraPixeles = 17000;
+	private int DistanciaCarreraPixeles = 10000;
 
 	public RaceAnimationPanel(Controller controller) {
 		super();
 		setBackground(Color.GRAY);		
-		
 		this.initComponents(controller);
 	}
 
@@ -40,6 +40,7 @@ public class RaceAnimationPanel extends JPanel {
 	 */
 	private void initComponents(Controller controller) {
 		setLayout(new BorderLayout());
+		validarTamanioPanel();
 		int sizeY = 357 ;		
 		raceSketch =  new RaceSketch(controller, DistanciaCarreraPixeles, sizeY);
 		raceSketch.setPreferredSize(new Dimension(DistanciaCarreraPixeles,sizeY));
@@ -47,6 +48,18 @@ public class RaceAnimationPanel extends JPanel {
 	}
 
 
+
+	private void validarTamanioPanel() {
+		if(RaceConstants.RACE_LENGTH >= 200000) {
+			this.DistanciaCarreraPixeles = 17000;
+		}else if(RaceConstants.RACE_LENGTH < 200000 && RaceConstants.RACE_LENGTH >= 50000  ) {
+			this.DistanciaCarreraPixeles = 15000;
+		}else if(RaceConstants.RACE_LENGTH < 50000 && RaceConstants.RACE_LENGTH > 10000  ) {
+			this.DistanciaCarreraPixeles = 10000;
+		}else if(RaceConstants.RACE_LENGTH <= 10000 && RaceConstants.RACE_LENGTH > 1000 ){
+			this.DistanciaCarreraPixeles = 3000;
+		}
+	}
 
 	/**
 	 * metodoi quye se llama desde el controler y actualiza la posicion de los racers
